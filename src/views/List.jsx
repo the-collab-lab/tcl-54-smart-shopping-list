90075ce845cc9b68d4d8bef5e9f6ddea46d26352
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-01-10 12:19:22 -0500

    rb + ad shopping list names rendered

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,18 +1,25 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	console.log(data);
+
+	// Q: const or let?
+	const listItem = data.map((item) => {
+		return <ListItem key={item.id} name={item.name} />;
+	});
+
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{/* work goes here* */}
 				{/**
 				 * TODO: write some JavaScript that renders the `data` array
 				 * using the `ListItem` component that's imported at the top
 				 * of this file.
 				 */}
+				{listItem}
 			</ul>
 		</>
 	);
