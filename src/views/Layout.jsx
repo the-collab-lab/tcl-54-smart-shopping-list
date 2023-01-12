e35259cd4601b3a9bc78e138cce41ad245517bee
Ashelle Cancio <vermillion707@gmail.com>
2023-01-12 15:29:49 -0800

    Modified Layout.jsx

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -2,14 +2,6 @@ import { Outlet, NavLink } from 'react-router-dom';
 
 import './Layout.css';
 
-/**
- * TODO: The links defined in this file don't work!
- *
- * Instead of anchor element, they should use a component
- * from `react-router-dom` to navigate to the routes
- * defined in `App.jsx`.
- */
-
 export function Layout() {
 	return (
 		<>
