a8b88857f2e90457b069040fb2ddd8751602ddb3
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-01-13 23:50:20 -0500

    cleaned up code, commented List component functionality, simplified how mapped data of shopping list items are rendered

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,25 +1,16 @@
 import { ListItem } from '../components';
 
+/** List component that displays items in a user's shopping cart  */
 export function List({ data }) {
-	console.log(data);
-
-	// Q: const or let?
-	const listItem = data.map((item) => {
-		return <ListItem key={item.id} name={item.name} />;
-	});
-
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
 			<ul>
-				{/**
-				 * TODO: write some JavaScript that renders the `data` array
-				 * using the `ListItem` component that's imported at the top
-				 * of this file.
-				 */}
-				{listItem}
+				{data.map((item) => {
+					return <ListItem key={item.id} name={item.name} />;
+				})}
 			</ul>
 		</>
 	);
