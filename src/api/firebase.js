58515ff4e87b3b27afc85c29e61e38e75cb4675e
Ashelle Cancio <vermillion707@gmail.com>
2023-01-17 13:48:23 -0800

    UI: Prepared form for item submission. #4

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,4 +1,4 @@
-import { collection, onSnapshot } from 'firebase/firestore';
+import { collection, onSnapshot, addDoc } from 'firebase/firestore';
 import { db } from './config';
 import { getFutureDate } from '../utils';
 
@@ -55,7 +55,7 @@ export async function addItem(listId, { itemName, daysUntilNextPurchase }) {
 	const listCollectionRef = collection(db, listId);
 	// TODO: Replace this call to console.log with the appropriate
 	// Firebase function, so this information is sent to your database!
-	return console.log(listCollectionRef, {
+	return addDoc(listCollectionRef, {
 		dateCreated: new Date(),
 		// NOTE: This is null because the item has just been created.
 		// We'll use updateItem to put a Date here when the item is purchased!
--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -1,3 +1,88 @@
-export function AddItem() {
-	return <p>Hello from the <code>/add-item</code> page!</p>
+import { addItem } from '../api/firebase';
+import { useState } from 'react';
+
+export function AddItem({ listId }) {
+	const [itemData, setItemData] = useState({
+		itemName: '',
+		daysUntilNextPurchase: null,
+	});
+	/*
+	itemData = {
+		itemName:
+		daysUntilNextPurchase:
+	}
+	*/
+
+	const onFormSubmit = (event) => {
+		event.preventDefault();
+		addItem(listId, itemData);
+		console.log(itemData);
+	};
+
+	const handleItemName = (event) => {
+		setItemData({
+			itemName: event.target.value,
+		});
+		console.log(event.target.value);
+	};
+
+	const handlePurchaseDate = (event) => {
+		let date = null;
+		if (event.target.name === 'soon') {
+			date = 7;
+			console.log('clicked 7');
+		} else if (event.target.name === 'kind-of-soon') {
+			date = 14;
+			console.log('clicked 14');
+		} else if (event.target.name === 'not-soon') {
+			date = 30;
+			console.log('clicked 30');
+		}
+		setItemData({
+			daysUntilNextPurchase: date,
+		});
+	};
+
+	return (
+		<form onSubmit={onFormSubmit}>
+			<div>Item name:</div>
+			<input type="text" name="item" onChange={handleItemName} />
+			<div>
+				<input
+					type="radio"
+					id="soon"
+					name="soon"
+					onChange={handlePurchaseDate}
+				/>
+				<label htmlFor="soon">Soon</label>
+			</div>
+			<div>
+				<input
+					type="radio"
+					id="kind-of-soon"
+					name="kind-of-soon"
+					onChange={handlePurchaseDate}
+				/>
+				<label htmlFor="kind-of-soon">Kind Of Soon</label>
+			</div>
+			<div>
+				<input
+					type="radio"
+					id="not-soon"
+					name="soon"
+					onChange={handlePurchaseDate}
+				/>
+				<label htmlFor="not-soon">Not Soon</label>
+			</div>
+			<button type="submit">Add Item</button>
+		</form>
+	);
 }
+// add radio buttons that correspond to soon, kind of soon, not soon
+// Add form that allows user to add item name
+// soon = 7 days
+// kind of soon = 14
+// not soon = 30
+
+// add label for each input (radio button)
+// submit button
