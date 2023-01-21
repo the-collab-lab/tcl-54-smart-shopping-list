9984a07f1121f7644c0dbbb88f1966c1d9f3fb24
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-01-21 13:49:02 -0500

    Added error handling for createNewList function with new dependency on 'react-hot-toast'. Added more descriptive comments.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,5 @@
 import './Home.css';
+import toast, { Toaster } from 'react-hot-toast';
 import { generateToken } from '@the-collab-lab/shopping-list-utils';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -8,8 +9,11 @@ import { createNewList } from '../api/firebase';
  * If the user doesn't already have a list, a user can create one to be saved to Firestore and be redirected to the List view. */
 
 export function Home({ setListToken }) {
+	// Declaring navigate for view redirection and errorNotify for error handling.
 	const navigate = useNavigate();
+	const errorNotify = () => toast.error('Error creating new shopping list ');
 
+	//Checks for shopping list token in local storage and redirects to list view if it exists.
 	const checkStorage = (key) => {
 		const storedToken = localStorage.getItem(key);
 		if (storedToken) {
@@ -17,15 +21,22 @@ export function Home({ setListToken }) {
 		}
 	};
 
+	//Calls checkStorage function when component is mounted.
 	useEffect(() => {
 		checkStorage('tcl-shopping-list-token');
 	}, []);
 
+	//Generates and saves new shopping list token.
 	const createNewToken = async () => {
 		const newToken = generateToken();
-		setListToken(newToken);
-		await createNewList(newToken);
-		navigate('/list');
+		createNewList(newToken)
+			.then(() => {
+				setListToken(newToken);
+				navigate('/list');
+			})
+			.catch((error) => {
+				errorNotify();
+			});
 	};
 
 	return (
@@ -34,6 +45,7 @@ export function Home({ setListToken }) {
 				Hello from the home (<code>/</code>) page!
 			</p>
 			<button onClick={createNewToken}>Create a new list</button>
+			<Toaster />
 		</div>
 	);
 }
