fcd6eccdc94600a32cc738bea22de83558f742fc
Ashelle Cancio <vermillion707@gmail.com>
2023-01-24 12:28:32 -0800

    Added join token form. #5

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,7 @@
 import './Home.css';
 import toast, { Toaster } from 'react-hot-toast';
 import { generateToken } from '@the-collab-lab/shopping-list-utils';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createNewList } from '../api/firebase';
 
@@ -9,6 +9,7 @@ import { createNewList } from '../api/firebase';
  * If the user doesn't already have a list, a user can create one to be saved to Firestore and be redirected to the List view. */
 
 export function Home({ setListToken }) {
+	const [joinToken, setJoinToken] = useState('');
 	// Declaring navigate for view redirection and errorNotify for error handling.
 	const navigate = useNavigate();
 	const errorNotify = () => toast.error('Error creating new shopping list ');
@@ -39,12 +40,36 @@ export function Home({ setListToken }) {
 			});
 	};
 
+	// Join existing shopping list
+	const joinExistingToken = async (e) => {
+		e.preventDefault();
+		console.log('token:', joinToken);
+	};
+
 	return (
 		<div className="Home">
 			<p>
 				Hello from the home (<code>/</code>) page!
 			</p>
 			<button onClick={createNewToken}>Create a new list</button>
+			<p>- or -</p>
+			<form onSubmit={joinExistingToken}>
+				<label htmlFor="join-token">Share Token</label>
+				<div>
+					<input
+						type="text"
+						name="token"
+						id="join-token"
+						placeholder="three word token"
+						onChange={(e) => setJoinToken(e.target.value)}
+					/>
+				</div>
+				<div>
+					<button type="submit" id="submit">
+						Join an existing list
+					</button>
+				</div>
+			</form>
 			<Toaster />
 		</div>
 	);
