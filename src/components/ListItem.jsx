ae74c99e4fa528b826a4fe9c4d2e8d7723a87c82
Ashelle Cancio <vermillion707@gmail.com>
2023-01-31 15:57:33 -0800

    Render checkbox with each item. #8

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,5 +1,12 @@
 import './ListItem.css';
 
 export function ListItem({ name }) {
-	return <li className="ListItem">{name}</li>;
+	return (
+		<li className="ListItem">
+			<label>
+				<input value={name} type="checkbox" />
+				{name}
+			</label>
+		</li>
+	);
 }
