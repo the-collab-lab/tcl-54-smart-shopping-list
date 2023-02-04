9d6f50c16744b7da2e75eeb5eafc643877b5ed60
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-02-04 12:08:36 -0500

    Add loading feature while list view is rendering. Also updated the logic for checking an empty list

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,16 +1,34 @@
 import { ListItem } from '../components';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 /** List component that displays items in a user's shopping cart  */
 export function List({ data }) {
 	const [filteredList, setFilteredList] = useState([]);
 	const [filterInput, setFilterInput] = useState('');
+	const [loading, setLoading] = useState(true);
 
 	/* Declare navigate for view redirection */
 	const navigate = useNavigate();
 
-	/* Use handler to change the state of filterInput  and convert all items to lowercase to facilitate a more thorough search */
+	/* the useEffect() hook ensures that a loading view is displayed for
+	one second while the correct return statement loads.*/
+	useEffect(() => {
+		setTimeout(() => {
+			setLoading(false);
+		}, 500);
+	}, []);
+
+	/* This function will return a boolean value of whether the list
+	is empty or not */
+	const checkForEmptyList = () => {
+		//return true if list is empty and false if list contains items
+		const shoppingListArr = data.map((item) => item.name);
+		return shoppingListArr.includes(undefined) ? true : false;
+	};
+
+	/* Use handler to change the state of filterInput 
+	and convert all items to lowercase to facilitate a more thorough search */
 	const handleInput = (event) => {
 		const value = event.target.value.toLowerCase().trim();
 		setFilterInput(value);
@@ -26,51 +44,58 @@ export function List({ data }) {
 		setFilterInput('');
 	};
 
+	/*Handles navigation to Add Item view */
 	const handleAddItem = () => {
-		/* navigate to add item view */
 		navigate('/add-item');
 	};
 
-	return data.length == 1 ? (
-		/* If list is empty, a welcoming user prompt is displayed to start adding items to the list */
-		<>
-			<p>
-				<strong>Add items to start your shopping list</strong>
-			</p>
-			<p>Once you add an item, your shopping list will appear here.</p>
-			<button onClick={handleAddItem}>Add items</button>
-		</>
-	) : (
-		/* If list contains items, the shopping list is displayed including the item filtering feature */
-		<>
-			<p>
-				Hello from the <code>/list</code> page!
-			</p>
+	if (loading) {
+		return <div>Loading...</div>;
+	} else {
+		console.log(checkForEmptyList());
+		return checkForEmptyList() ? (
+			/* If list is returns true that it is empty, 
+			a welcoming user prompt is displayed to start adding items to the list */
+			<>
+				<p>
+					<strong>Add items to start your shopping list</strong>
+				</p>
+				<p>Once you add an item, your shopping list will appear here.</p>
+				<button onClick={handleAddItem}>Add items</button>
+			</>
+		) : (
+			/* If list returns false meaning it contains items,
+			 the shopping list is displayed including the item filtering feature */
+			<>
+				<p>
+					Hello from the <code>/list</code> page!
+				</p>
 
-			<form>
-				<label htmlFor="list-filter">Filter items</label>
-				<br />
-				<div>
-					<input
-						id="list-filter"
-						type="text"
-						placeholder="Start typing here..."
-						value={filterInput}
-						onChange={handleInput}
-					/>
-					{filterInput && <button onClick={handleClick}>X</button>}
-				</div>
-			</form>
-			{/* Uses data or state of filteredList depending on state of filterInput */}
-			<ul>
-				{!filterInput
-					? data.map((item) => {
-							return <ListItem key={item.id} name={item.name} />;
-					  })
-					: filteredList.map((item) => {
-							return <ListItem key={item.id} name={item.name} />;
-					  })}
-			</ul>
-		</>
-	);
+				<form>
+					<label htmlFor="list-filter">Filter items</label>
+					<br />
+					<div>
+						<input
+							id="list-filter"
+							type="text"
+							placeholder="Start typing here..."
+							value={filterInput}
+							onChange={handleInput}
+						/>
+						{filterInput && <button onClick={handleClick}>X</button>}
+					</div>
+				</form>
+				{/* Uses data or state of filteredList depending on state of filterInput */}
+				<ul>
+					{!filterInput
+						? data.map((item) => {
+								return <ListItem key={item.id} name={item.name} />;
+						  })
+						: filteredList.map((item) => {
+								return <ListItem key={item.id} name={item.name} />;
+						  })}
+				</ul>
+			</>
+		);
+	}
 }
