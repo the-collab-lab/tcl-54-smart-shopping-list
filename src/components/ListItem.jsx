11142c3140d10e946d62e92abc688fdec80acf3e
Ashelle Cancio <vermillion707@gmail.com>
2023-02-04 10:01:47 -0800

    Clean up: comments #8

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -36,65 +36,6 @@ export function ListItem({ name, itemId, dateLastPurchased }) {
 		}
 	};
 
-	// useEffect(() => {
-	// 	const listId = localStorage.getItem('tcl-shopping-list-token');
-
-	// 	if (check) {
-	// 		updateItem(listId, itemId, check);
-	// 	} else {
-	// 		updateItem(listId, itemId, check);
-	// 	}
-	// }, [check, itemId]);()
-
-	// const toggle = (value) => {
-	// 	console.log('value:', value);
-	// 	return !value;
-	// };
-
-	// const handleCheck = async (e) => {
-	// 	const listId = localStorage.getItem('tcl-shopping-list-token');
-	// 	const value = toggle(check);
-	// 	if (e.target.checked) {
-	// 		setCheck(value);
-	// 		console.log('check:', check);
-	// 	} else {
-	// 		setCheck(value);
-	// 		console.log('check:', check);
-	// 	}
-	// 	await updateItem(listId, itemId, check);
-	// };
-
-	// const handleCheck = async (e) => {
-	// 	const listId = localStorage.getItem('tcl-shopping-list-token');
-	// 	setCheck(!check)
-	// 		.then(() => {
-	// 			updateItem(listId, itemId, check);
-	// 		})
-	// 		.catch((error) => {
-	// 			console.log(error);
-	// 		});
-	// };
-
-	// const handleCheck = (e) => {
-	// 	const listId = localStorage.getItem('tcl-shopping-list-token');
-	// 	if (e.target.checked) {
-	// 		setCheck(true);
-	// 		updateItem(listId, itemId, check);
-	// 	} else {
-	// 		setCheck(false);
-	// 	}
-	// };
-
-	// const handleCheck = (e) => {
-	// 	const listId = localStorage.getItem('tcl-shopping-list-token');
-	// 	if (check === false) {
-	// 		setCheck(true);
-	// 		updateItem(listId, item);
-	// 	} else {
-	// 		setCheck(false);
-	// 	}
-	// };
-
 	if (name) {
 		return (
 			<li className="ListItem">
