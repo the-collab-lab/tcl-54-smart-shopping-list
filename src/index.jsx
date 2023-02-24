d5821b5c4a72eff99876d877367f9a2fc049e762
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-02-24 18:04:30 -0500

    no list view page updated, commented out overlapping css styling in index #34

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
+// import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
