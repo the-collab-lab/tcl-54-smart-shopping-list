94d5aeb76d0e56c44778b8019530e20aa28ccf6d
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-02-25 06:47:49 -0500

    Layout.jsx uupdated to show shopping list token on list view with copy icon #34

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,18 +1,48 @@
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import React, { useState } from 'react';
+import { Image } from 'react-bootstrap';
 
 // import './Layout.css';
 import { NavbarShoppingList } from '../components';
 
-export function Layout() {
+export function Layout({ listToken }) {
+	// useLocation hook will be used to check current route
 	const location = useLocation();
+	const [copied, setCopied] = useState(false);
+
+	if (location.pathname === '/list') {
+		return (
+			<React.Fragment>
+				<div className="Layout">
+					<header className="Layout-header">
+						<h4>Current List:</h4>
+						<h1>
+							{listToken}
+							<span>
+								<CopyToClipboard text="Hello!">
+									<Image type="button" src="../img/icons/copy-icon.svg" />
+								</CopyToClipboard>
+							</span>
+						</h1>
+					</header>
+					<main className="Layout-main">
+						<Outlet />
+					</main>
+				</div>
+			</React.Fragment>
+		);
+	}
+
 	return (
-		<>
-			{location.pathname !== '/' && <NavbarShoppingList />}
-			<div className="Layout">
-				<main className="Layout-main">
-					<Outlet />
-				</main>
-			</div>
-		</>
+		<div className="Layout">
+			<header className="Layout-header">
+				<h1>Smart shopping list</h1>
+			</header>
+			<main className="Layout-main">
+				<Outlet />
+			</main>
+		</div>
 	);
 }
