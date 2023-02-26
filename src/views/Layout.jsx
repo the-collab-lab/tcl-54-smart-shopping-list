214d02563030b04f4e6a156ec5c49e1710d08ed1
Ashelle Cancio <vermillion707@gmail.com>
2023-02-26 15:55:05 -0800

    Moved `NavBarShoppingList` to `Layout.jsx` for `useLocation()` to work properly. #35

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -1,10 +1,13 @@
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import './Layout.css';
+import { NavbarShoppingList } from '../components';
 
 export function Layout() {
+	const location = useLocation();
 	return (
 		<>
+			{location.pathname !== '/' && <NavbarShoppingList />}
 			<div className="Layout">
 				<header className="Layout-header">
 					<h1>Smart shopping list</h1>
