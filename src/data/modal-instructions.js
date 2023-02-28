bbcf2a99f901a26ddb09672957c158ba8f6fa338
Ashelle Cancio <vermillion707@gmail.com>
2023-02-27 17:21:19 -0800

    Test commit 3

---
+++ src/data/modal-instructions.js
@@ -0,0 +1,11 @@
+export const instructions = [
+	{
+		text: '1. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque explicabo excepturi minima fuga exercitationem perferendis. Vero aliquid iure dignissimos incidunt.',
+	},
+	{
+		text: '2. Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+	},
+	{
+		text: '3. Atque explicabo excepturi minima fuga exercitationem perferendis. Vero aliquid iure dignissimos incidunt.',
+	},
+];
