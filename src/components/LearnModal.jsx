abe94014995d94548ec3ec4c596e9a8902018115
Ashelle Cancio <vermillion707@gmail.com>
2023-02-27 17:39:53 -0800

    Test commit 4

--- src/components/LearnModal.jsx
+++ src/components/LearnModal.jsx
@@ -1,18 +1,6 @@
 import { Button, Modal } from 'react-bootstrap';
-import { useState } from 'React';
+import { useState } from 'react';
 import { instructions } from '../data/modal-instructions';
-
-// const instructions = [
-// 	{
-// 		text: '1. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque explicabo excepturi minima fuga exercitationem perferendis. Vero aliquid iure dignissimos incidunt.',
-// 	},
-// 	{
-// 		text: '2. Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
-// 	},
-// 	{
-// 		text: '3. Atque explicabo excepturi minima fuga exercitationem perferendis. Vero aliquid iure dignissimos incidunt.',
-// 	},
-// ];
 
 export default function LearnModal({ show, hide }) {
 	const [currentIndex, setCurrentIndex] = useState(0);
