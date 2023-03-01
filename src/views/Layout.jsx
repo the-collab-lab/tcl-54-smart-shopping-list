6b45dd6e27cc7681e0365178c736654706f97f69
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2023-03-01 17:58:41 -0500

    cleaned comments and css files

--- src/views/Layout.jsx
+++ src/views/Layout.jsx
@@ -10,36 +10,35 @@ import './Layout.css';
 export function Layout({ listToken }) {
 	// useLocation hook will be used to check current route
 	const location = useLocation();
-	const [copied, setCopied] = useState(false);
 
 	// Notify when adding item is unsuccessful
 	const notifyCopied = () => toast('Copied!');
 
+	// List view header will display if on list pathname
+	// to show the current list token of the use with copy functionality
 	if (location.pathname === '/list') {
 		return (
-			<React.Fragment>
+			<div className="List">
 				<Toaster />
-				<div className="List">
-					<header className="List-header">
-						<h4>Current List:</h4>
-						<h1>
-							{listToken}
-							<span>
-								<CopyToClipboard text={listToken}>
-									<Image
-										type="button"
-										src="../img/icons/copy-icon.svg"
-										onClick={notifyCopied}
-									/>
-								</CopyToClipboard>
-							</span>
-						</h1>
-					</header>
-					<main className="Layout-main">
-						<Outlet />
-					</main>
-				</div>
-			</React.Fragment>
+				<header className="List-header">
+					<h4>Current List:</h4>
+					<h1>
+						{listToken}
+						<span>
+							<CopyToClipboard text={listToken}>
+								<Image
+									type="button"
+									src="../img/icons/copy-icon.svg"
+									onClick={notifyCopied}
+								/>
+							</CopyToClipboard>
+						</span>
+					</h1>
+				</header>
+				<main className="Layout-main">
+					<Outlet />
+				</main>
+			</div>
 		);
 	}
 
