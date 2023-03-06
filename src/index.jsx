aa505cbddbefacdb7bf3415410c8b67c9fe10fdd
Ashelle Cancio <vermillion707@gmail.com>
2023-03-06 02:12:49 -0800

    Modified `index.jsx` to fix the DOM element error for the production build. #54

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,26 @@
+/** This modified code block waits for the DOM to fully load
+ * before mounting the React app to the `root` element.
+ * Seems to solve the prior error we were having that prevented
+ * the app to load properyly on refreshes.
+ */
+
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+const mountApp = () => {
+	const root = document.getElementById('root');
+	if (root) {
+		createRoot(root).render(<App />);
+	} else {
+		console.error('Could not find root element');
+	}
+};
+
+if (document.readyState === 'loading') {
+	document.addEventListener('DOMContentLoaded', mountApp);
+} else {
+	mountApp();
+}
